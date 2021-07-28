267b96c9f73b35907760854fd44c4f726ab1d814
Vinícius de Oliveira Souza <viniciusoliveira@ciandt.com>
2021-07-28 10:51:27 -0300

    Adding pre commmit

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 
 phpcs --config-set installed_paths ../../drupal/coder/coder_sniffer
 cp docroot/sites/example.settings.local.php docroot/sites/default/settings.local.php
+cp pre-commit.sh .git/hooks/pre-commit
